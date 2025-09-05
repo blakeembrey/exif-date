25fb8b4d3a32bde1a5208a055a0ac15d86d89251
Liam Morley <liam@carpeliam.com>
2025-09-05 14:49:05 -0400

    Fix offset issue where timezone offsets were inverted (#18)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,23 +2,23 @@ import { format, parse } from "./index";
 
 describe("exif date", () => {
   it("should parse", () => {
-    expect(parse("2016:07:11 21:33:20").toISOString()).toEqual(
-      "2016-07-11T21:33:20.000Z"
+    expect(parse("2016:07:11 21:33:20")).toEqual(
+      new Date("2016-07-11T21:33:20.000Z")
     );
-    expect(parse("2016:07:11 21:33:20.71").toISOString()).toEqual(
-      "2016-07-11T21:33:20.071Z"
+    expect(parse("2016:07:11 21:33:20.71")).toEqual(
+      new Date("2016-07-11T21:33:20.071Z")
     );
-    expect(parse("2015:03:19 19:03:50Z").toISOString()).toEqual(
-      "2015-03-19T19:03:50.000Z"
+    expect(parse("2015:03:19 19:03:50Z")).toEqual(
+      new Date("2015-03-19T19:03:50.000Z")
     );
-    expect(parse("2013:10:06 18:55:39.114").toISOString()).toEqual(
-      "2013-10-06T18:55:39.114Z"
+    expect(parse("2013:10:06 18:55:39.114")).toEqual(
+      new Date("2013-10-06T18:55:39.114Z")
     );
-    expect(parse("2016:08:22 13:41:47+10:00").toISOString()).toEqual(
-      "2016-08-22T23:41:47.000Z"
+    expect(parse("2016:08:22 13:41:47+10:00")).toEqual(
+      new Date("2016-08-22T13:41:47.000+10:00")
     );
-    expect(parse("2016:08:22 13:41:47-10:00").toISOString()).toEqual(
-      "2016-08-22T03:41:47.000Z"
+    expect(parse("2016:08:22 13:41:47-10:00")).toEqual(
+      new Date("2016-08-22T13:41:47.000-10:00")
     );
     expect(parse("not a date").getTime()).toEqual(NaN);
   });
